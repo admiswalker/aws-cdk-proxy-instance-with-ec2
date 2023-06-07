46502f42cf27e8f7664c273c0c91cabd1b693bd1
admiswalker <admiswalker@gmail.com>
2023-06-07 23:25:00 +0900

    split proxy-instance

--- lib/aws-cdk-proxy-instance-with-ec2-stack.ts
+++ lib/aws-cdk-proxy-instance-with-ec2-stack.ts
@@ -6,6 +6,8 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 
 import * as fs from 'fs';
 
+import { ProxyInstance } from '../lib/proxy-instance';
+
 interface PoxyInstTestStackProps extends cdk.StackProps {
 }
 export class PoxyInstTestStack extends cdk.Stack {
@@ -42,54 +44,11 @@ export class PoxyInstTestStack extends cdk.Stack {
       vpc: vpc,
     });
 
-    // SSM
-    const proxy_iam_role = new iam.Role(this, 'iam_role_for_proxy_ssm', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
-        iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchAgentAdminPolicy'),
-      ],
+    const proxy_inst = new ProxyInstance(this, 'ProxyInstance', {
+      sg: proxy_sg,
+      stack_name: PoxyInstTestStack.name,
+      vpc: vpc,
     });
-    const iamInstanceProfile = new iam.CfnInstanceProfile(this, 'InstanceProfile', {
-      path: '/',
-      roles: [proxy_iam_role.roleName]
-    });
-  
-    // Proxy Instance
-    const machineImageId = ec2.MachineImage.latestAmazonLinux({
-      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      edition: ec2.AmazonLinuxEdition.STANDARD,
-      virtualization: ec2.AmazonLinuxVirt.HVM,
-      //cpuType: ec2.AmazonLinuxCpuType.X86_64,
-      cpuType: ec2.AmazonLinuxCpuType.ARM_64,
-    }).getImage(this).imageId;
-
-    const instance = new ec2.CfnInstance(this, 'ProxyInstance', {
-      blockDeviceMappings: [{
-        deviceName: '/dev/xvda',
-          ebs: {
-            deleteOnTermination: true,
-            encrypted: true,
-            volumeSize: 8,
-            volumeType: ec2.EbsDeviceVolumeType.GENERAL_PURPOSE_SSD_GP3, // ref: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.EbsDeviceVolumeType.html
-            //volumeType: ec2.EbsDeviceVolumeType.STANDARD, // ref: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.EbsDeviceVolumeType.html
-        }
-      }],
-      imageId: machineImageId,
-      //instanceType: 't3a.nano', // 2 vCPU, 0.5 GB (AMD)
-      instanceType: 't4g.nano', // 2 vCPU, 0.5 GB (ARM)
-      iamInstanceProfile: iamInstanceProfile.ref,
-      securityGroupIds: [proxy_sg.securityGroupId],
-      //sourceDestCheck: false, // Required by NAT Instance Operation
-      subnetId: vpc.publicSubnets[0].subnetId,
-      privateIpAddress: '10.0.0.24', // IP address specification
-      userData: cdk.Fn.base64(fs.readFileSync('./lib/user-data-proxy.yaml', 'utf8')),
-      tags: [{
-        "key": "Name",
-        "value": PoxyInstTestStack.name+"/ProxyInstance"
-      }]
-    });
-    const instanceId = instance.ref;
 
     //---------------------------------------------------------------------- end ---
 
